8057d0c3a2c5090ae189496a15ea800bf76d49e0
Zychkov <48855208+Zychkov@users.noreply.github.com>
2021-07-31 18:16:38 +0700

    spring_data_jpa

---
+++ src/main/java/ru/zychkov/spring/springboot/spring_data_jpa/controller/MyRestController.java
@@ -0,0 +1,67 @@
+package ru.zychkov.spring.springboot.spring_data_jpa.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.zychkov.spring.springboot.spring_data_jpa.entity.Employee;
+import ru.zychkov.spring.springboot.spring_data_jpa.service.EmployeeService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class MyRestController {
+
+    @Autowired
+    private EmployeeService employeeService;
+
+    @GetMapping("/employees")
+    public List<Employee> showAllEmployees() {
+        List<Employee> allEmployees = employeeService.getAllEmployees();
+
+        return allEmployees;
+    }
+
+    @GetMapping("/employees/{id}")
+    public Employee getEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+
+        return employee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+
+        return employee;
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        Employee employee = employeeService.getEmployee(id);
+
+        employeeService.deleteEmployee(id);
+
+        return "Employee with ID = " + id + " was deleted";
+    }
+
+    @GetMapping("/employees/name/{name}")
+    public List<Employee> showAllEmployeesByName(@PathVariable String name) {
+        List<Employee> employees = employeeService.findAllByName(name);
+
+        return employees;
+    }
+}
